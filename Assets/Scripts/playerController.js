0ede523494e30beeb19dfedbdc3e391f4175fa21
Emerson Shen <emersonshen@Emersons-MacBook-Pro.local>
2017-03-11 14:58:06 -0500

    Adding Basic Player Movement

---
+++ Assets/Scripts/playerController.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var speed = 2.0;
+private var rb : Rigidbody2D;
+
+function Start () {
+	rb = GetComponent.<Rigidbody2D>();
+}
+
+function Update () {
+	var direction = Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+	rb.AddForce(direction * speed);
+	//transform.Translate(direction * speed * Time.deltaTime);
+
+}
