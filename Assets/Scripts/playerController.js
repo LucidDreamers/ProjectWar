047c5c49de58d4ec064db63e27f72bc042444404
Emerson <emersonshen@gmail.com>
2017-03-14 14:45:20 -0400

    Added Player movement

--- Assets/Scripts/playerController.js
+++ Assets/Scripts/playerController.js
@@ -1,16 +1,28 @@
 ﻿#pragma strict
 
 public var speed = 2.0;
+public var bullet : GameObject;
+public var bulletSpawn : GameObject;
 private var rb : Rigidbody2D;
+
 
 function Start () {
 	rb = GetComponent.<Rigidbody2D>();
 }
 
 function Update () {
+
+	/*var pos = Camera.main.WorldToScreenPoint(transform.position);
+	var dir = Input.mousePosition - pos;
+	var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+	transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);*/
+
 	var direction = Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-	rb.AddForce(direction * speed);
-	//transform.Translate(direction * speed * Time.deltaTime);
+	rb.AddForce(direction * speed, ForceMode2D.Force);
+
+	/*if(Input.GetKeyDown("Fire1")){
+		var bulletClone = Instantiate(bullet, Vector2(bulletSpawn.position.X, bulletSpawn.position.Y));
+	}*/
 
 }
