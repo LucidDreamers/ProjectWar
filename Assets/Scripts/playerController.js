fe45685e5f1ab0f1b1a5622f0e2242c71cf14e5d
Emerson <emersonshen@gmail.com>
2017-03-23 21:17:05 -0400

    Player Follows Mouse and can Fire Basic Projectile

--- Assets/Scripts/playerController.js
+++ Assets/Scripts/playerController.js
@@ -1,7 +1,8 @@
 ﻿#pragma strict
 
 public var speed = 2.0;
-public var bullet : GameObject;
+public var bulletSpeed = 6;
+public var bulletPrefab : GameObject;
 public var bulletSpawn : GameObject;
 private var rb : Rigidbody2D;
 
@@ -11,18 +12,22 @@ function Start () {
 }
 
 function Update () {
+	/*if(Input.GetJoystickNames != null){
 
-	/*var pos = Camera.main.WorldToScreenPoint(transform.position);
+	}*/
+	var pos = Camera.main.WorldToScreenPoint(transform.position);
 	var dir = Input.mousePosition - pos;
-	var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-	transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);*/
+	var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+	transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
 	var direction = Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
 	rb.AddForce(direction * speed, ForceMode2D.Force);
 
-	/*if(Input.GetKeyDown("Fire1")){
-		var bulletClone = Instantiate(bullet, Vector2(bulletSpawn.position.X, bulletSpawn.position.Y));
-	}*/
+	if(Input.GetButtonDown("Fire1")){
+		var bullet : GameObject = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation) as GameObject;
+		bullet.GetComponent(Rigidbody2D).AddForce(bulletSpawn.transform.up * bulletSpeed, ForceMode2D.Impulse);
+		Destroy(bullet, 2.0f);
+	}
 
 }
