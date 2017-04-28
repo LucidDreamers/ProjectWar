368e4da3e56d3127d41040c6ab531285b8de50e2
Emerson Shen <Emerson Shen>
2017-04-28 17:21:52 -0400

    Added hold key, fire rate and max projectile on screen

--- Assets/Scripts/playerController.js
+++ Assets/Scripts/playerController.js
@@ -4,13 +4,16 @@ public var bulletPrefab : GameObject;
 public var bulletSpawn1 : GameObject;
 public var bulletSpawn2 : GameObject;
 private var rb : Rigidbody2D;
+private var gos : GameObject[];
 private var stats : playerStatController;
 private var toggle = 0;
+private var allowFire = true;
 
 
 function Start () {
 	rb = GetComponent(Rigidbody2D);
 	stats = GetComponent(playerStatController);
+	gos = gameObject.FindGameObjectsWithTag("Projectile");
 }
 
 function OnCollisionEnter2D (collision : Collision2D){
@@ -37,6 +40,7 @@ function Update () {
 
 	}*/
 
+	gos = gameObject.FindGameObjectsWithTag("Projectile");
 	//This makes the player ship follow the mouse
 	//Determine the position of the ship and subtract it from the mouse position
 	//Then determine the rotation angle and rotate the player
@@ -50,32 +54,37 @@ function Update () {
 	var direction = Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 	rb.AddForce(direction * stats.speed, ForceMode2D.Force);
 
-	if(Input.GetButtonDown("Fire1")){
-		var bullet : GameObject;
-
-		//Double shot mode
-		if(toggle == 2){
-			var bullet1 : GameObject = Instantiate(bulletPrefab, bulletSpawn1.transform.position, bulletSpawn1.transform.rotation) as GameObject;
-			bullet1.GetComponent(Rigidbody2D).AddForce(bulletSpawn1.transform.up * stats.bulletSpeed, ForceMode2D.Impulse);
-			var bullet2 : GameObject = Instantiate(bulletPrefab, bulletSpawn2.transform.position, bulletSpawn2.transform.rotation) as GameObject;
-			bullet2.GetComponent(Rigidbody2D).AddForce(bulletSpawn2.transform.up * stats.bulletSpeed, ForceMode2D.Impulse);
-			Destroy(bullet1, 2.0f);
-			Destroy(bullet2, 2.0f);
-		}
-		//Single shot mode
-		//uses a variable that toggles when one side has shot to shoot from the other point the next time
-		else if(toggle == 1){
-			bullet = Instantiate(bulletPrefab, bulletSpawn2.transform.position, bulletSpawn2.transform.rotation) as GameObject;
-			bullet.GetComponent(Rigidbody2D).AddForce(bulletSpawn2.transform.up * stats.bulletSpeed, ForceMode2D.Impulse);
-			Destroy(bullet, 2.0f);
-			toggle = 0;
-		}else{
-			bullet = Instantiate(bulletPrefab, bulletSpawn1.transform.position, bulletSpawn1.transform.rotation) as GameObject;
-			bullet.GetComponent(Rigidbody2D).AddForce(bulletSpawn1.transform.up * stats.bulletSpeed, ForceMode2D.Impulse);
-			Destroy(bullet, 2.0f);
-			toggle = 1;
-		}
+	if(Input.GetButton("Fire1") && gos.length < stats.bulletLimit && allowFire){
+		Fire();
 
 	}
 
+}
+function Fire (){
+	var bullet : GameObject;
+	allowFire = false;
+	//Double shot mode
+	if(toggle == 2){
+		var bullet1 : GameObject = Instantiate(bulletPrefab, bulletSpawn1.transform.position, bulletSpawn1.transform.rotation) as GameObject;
+		bullet1.GetComponent(Rigidbody2D).AddForce(bulletSpawn1.transform.up * stats.bulletSpeed, ForceMode2D.Impulse);
+		var bullet2 : GameObject = Instantiate(bulletPrefab, bulletSpawn2.transform.position, bulletSpawn2.transform.rotation) as GameObject;
+		bullet2.GetComponent(Rigidbody2D).AddForce(bulletSpawn2.transform.up * stats.bulletSpeed, ForceMode2D.Impulse);
+		Destroy(bullet1, 2.0f);
+		Destroy(bullet2, 2.0f);
+	}
+	//Single shot mode
+	//uses a variable that toggles when one side has shot to shoot from the other point the next time
+	else if(toggle == 1){
+		bullet = Instantiate(bulletPrefab, bulletSpawn2.transform.position, bulletSpawn2.transform.rotation) as GameObject;
+		bullet.GetComponent(Rigidbody2D).AddForce(bulletSpawn2.transform.up * stats.bulletSpeed, ForceMode2D.Impulse);
+		Destroy(bullet, 2.0f);
+		toggle = 0;
+	}else{
+		bullet = Instantiate(bulletPrefab, bulletSpawn1.transform.position, bulletSpawn1.transform.rotation) as GameObject;
+		bullet.GetComponent(Rigidbody2D).AddForce(bulletSpawn1.transform.up * stats.bulletSpeed, ForceMode2D.Impulse);
+		Destroy(bullet, 2.0f);
+		toggle = 1;
+	}
+	yield WaitForSeconds(stats.fireRate);
+	allowFire = true;
 }
--- Assets/Scripts/playerStatController.js
+++ Assets/Scripts/playerStatController.js
@@ -4,6 +4,8 @@ public var speed = 20f;
 public var bulletSpeed = 20f;
 public var health = 10f;
 public var bulletDamage = 10f;
+public var bulletLimit = 5;
+public var fireRate = 0.25f;
 
 function Start () {
 
